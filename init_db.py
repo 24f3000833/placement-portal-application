33f7eb81b18033227e872ed7bf158f8b45375a0e
anshul pandey <Anshul Pandey>
2026-06-19 17:56:08 +0530

    Updated admin credentials

--- init_db.py
+++ init_db.py
@@ -1,15 +1,19 @@
 from app import app , db
 from models import Admin
 from werkzeug.security import generate_password_hash
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 with app.app_context():
     db.create_all()
 
-    admin=Admin.query.filter_by(username="admin").first()
+    admin = Admin.query.filter_by(username=os.environ.get("ADMIN_USERNAME")).first()
 
     if not admin:
-        hashed_pass=generate_password_hash("admin123")
-        admin=Admin(username="admin",password=hashed_pass) #Stores admin detail in admin table
+        hashed_pass=generate_password_hash(os.environ.get("ADMIN_PASSWOD"))
+        admin=Admin(username=os.environ.get("ADMIN_USERNAME"), password=hashed_pass) #Stores admin detail in admin table
         db.session.add(admin)
         db.session.commit()
         print("Databse created Successfully...\nPrint Admin account created..")
